b3c854a17fbf7edfbb1bedfe40b8d7156d2c0bf2
Aaron Mackay <aaronmackay@hotmail.com>
2020-12-15 13:52:47 +0000

    Added events for server emits

--- server/index.js
+++ server/index.js
@@ -21,10 +21,25 @@ io.on("connection", socket => {
     const { id } = socket.client;
     console.log(`User connected: ${id}`);
 
+    socket.on("login", username => {
+        console.log(username, " logged in")
+        io.emit("login", username);
+    })
+
     socket.on("chat message", msg => {
         console.log(`${id}: ${msg}`);
     });
+
+    socket.on("disconnect", () => {
+        console.log("disconnected");
+        // emit that user disconnected
+        io.emit("disconnected", id)
+    }
+    )
+
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
