cdb90f1ae161de042fcb6f59e197a4f9766920e3
Aaron Mackay <aaronmackay@hotmail.com>
2020-12-15 15:08:19 +0000

    Added online users function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:5000");
 
-function App() {
-  return <div />;
+class App extends Component {
+  // Add constructor to initiate
+  constructor() {
+    super();
+    this.state = {
+      msg: "",
+      username: "",
+      onlineUsers: [],
+      chat: [],
+    };
+  }
+
+  componentDidMount() {
+    socket.on("loginState", (user) => {
+      // Add new messages to existing messages in "chat"
+      this.setState({
+        onlineUsers: [...this.state.onlineUsers, user]
+      });
+    });
+
+    socket.on("disconnected", (id) => {
+      // Add new messages to existing messages in "chat"
+      this.setState({
+        onlineUsers: [...this.state.onlineUsers].filter(x => x.id !== id)
+      });
+    });
+  }
+
+  // insert setStates
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
